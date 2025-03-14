64674d4e712d840e62e8b6a48fb12dac9e020b80
asifjawedgoodbot <128465405+asifjawedgoodbot@users.noreply.github.com>
2025-03-14 14:56:49 +0100

    removed redirect from signin function as supabase doc

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -173,9 +173,9 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
-        options: {
-          redirectTo: `https://branded-avatar-creator.lovable.app/auth/callback`,
-        },
+        // options: {
+        //   redirectTo: `https://branded-avatar-creator.lovable.app/auth/callback`,
+        // },
       });
       
       if (error) throw error;
